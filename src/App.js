a66b68f8d39919dd3bfb84235726e6e1c4c7b227
Marley Alford <marley.alford@mountsinai.org>
2020-02-08 15:39:35 -0500

    Added Post component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Post from "./components/Post";
 
 class App extends React.Component {
   render() {
-    return <Header />;
+    return (
+      <div>
+        <Header />
+        <div>
+          <Post />
+        </div>
+      </div>
+    );
   }
 }
 
