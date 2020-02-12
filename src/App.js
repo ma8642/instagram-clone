4f2efb7181ec517d3acb7e18f26396652e6c42a4
Marley Alford <marley.alford@mountsinai.org>
2020-02-11 21:24:17 -0500

    Pusher and apollo client set up

--- src/App.js
+++ src/App.js
@@ -8,19 +8,29 @@ import Header from "./components/Header";
 import Posts from "./components/Posts";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import Pusher from "pusher-js";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
 class App extends React.Component {
+  constructor() {
+    super();
+    //connect to pusher
+    this.pusher = new Pusher("PUSHER_APP_KEY", {
+      cluster: "eu",
+      encrypted: true
+    });
+  }
   render() {
     return (
       <ApolloProvider client={client}>
         <div className="App">
           <Header />
           <section className="App-main">
-            <Posts apollo_client={client} />
+            {/* pass the pusher object and apollo to the posts component */}
+            <Posts pusher={this.pusher} apollo_client={client} />
           </section>
         </div>
       </ApolloProvider>
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,48 +1,65 @@
 // src/components/Posts/index.js
-import React from "react";
+import React, { Component } from "react";
 import "./Posts.css";
-import { Query } from "react-apollo";
+// import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Post from "../Post";
 
-const Posts = () => {
-  return (
-    <Query
-      query={gql`
-        {
-          posts(user_id: "a") {
-            id
-            user {
-              nickname
-              avatar
+class Posts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    // fetch the initial posts
+    console.log(this.props);
+    this.props.apollo_client
+      .query({
+        query: gql`
+          {
+            posts(user_id: "a") {
+              id
+              user {
+                nickname
+                avatar
+              }
+              image
+              caption
             }
-            image
-            caption
           }
-        }
-      `}
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading Posts...</p>;
-        if (error) return <p>Error Fetching Posts...</p>;
-        let posts = data.posts;
-
-        return (
-          <div className="Posts">
-            {posts.map(post => (
-              <Post
-                nickname={post.user.nickname}
-                avatar={post.user.avatar}
-                image={post.image}
-                caption={post.caption}
-                key={post.id}
-              />
-            ))}
-          </div>
-        );
-      }}
-    </Query>
-  );
-};
+        `
+      })
+      .then(response => {
+        this.setState({ posts: response.data.posts });
+      });
+    // subscribe to posts channel
+    this.posts_channel = this.props.pusher.subscribe("posts-channel");
+    // listen for new posts
+    this.posts_channel.bind(
+      "new-post",
+      data => {
+        this.setState({ posts: this.state.posts.concat(data.post) });
+      },
+      this
+    );
+  }
+  render() {
+    return (
+      <div className="Posts">
+        {this.state.posts.map(post => (
+          <Post
+            nickname={post.user.nickname}
+            avatar={post.user.avatar}
+            image={post.image}
+            caption={post.caption}
+            key={post.id}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Posts;
